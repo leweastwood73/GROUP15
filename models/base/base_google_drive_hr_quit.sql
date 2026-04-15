f745d3ae39eb9a847f389244711804872d8f870f
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 07:40:16 -0400

    base_google_drive_hr_quit.sql

--- models/base/base_google_drive_hr_quit.sql
+++ models/base/base_google_drive_hr_quit.sql
@@ -1,8 +1,8 @@
-SELECT
-    _FILE AS file,
-    _LINE AS line,
-    _MODIFIED AS modified,
-    _FIVETRAN_SYNCED AS fivetran_synced,
-    CAST(EMPLOYEE_ID AS NUMBER) AS employee_id,
-    TRY_TO_DATE(QUIT_DATE) AS quit_date
+SELECT 
+    _FILE AS file, 
+    _LINE AS line, 
+    _MODIFIED AS modified, 
+    _FIVETRAN_SYNCED AS fivetran_synced, 
+    CAST(EMPLOYEE_ID AS NUMBER) AS employee_id, 
+    CAST(QUIT_DATE AS DATE) AS quit_date 
 FROM LOAD.GOOGLE_DRIVE.HR_QUITS
