6139edb2b938d704707e2b382fb6ba5336763b92
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 11:13:56 -0400

    Update base_web_schema_sessions.sql

--- models/base/base_web_schema_sessions.sql
+++ models/base/base_web_schema_sessions.sql
@@ -1,8 +1,9 @@
 SELECT
-    "_fivetran_id" AS fivetran_id,
+    CLIENT_ID AS client_id,
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM(SESSION_ID), '^s', '')) AS session_id,
-    SESSION_START_AT AS session_start_at,
-    SESSION_END_AT AS session_end_at,
+    LOWER(TRIM(OS)) AS os,
+    TRIM(IP) AS ip,
+    SESSION_AT AS session_at,
     "_fivetran_deleted" AS fivetran_deleted,
     "_fivetran_synced" AS fivetran_synced
 FROM LOAD.WEB_SCHEMA.SESSIONS
