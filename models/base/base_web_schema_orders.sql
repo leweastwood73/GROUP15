64627d91aac52300c7282e497d4e2a18a4cef0a0
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 10:49:14 -0400

    Update base_web_schema_orders.sql

--- models/base/base_web_schema_orders.sql
+++ models/base/base_web_schema_orders.sql
@@ -5,12 +5,12 @@ SELECT
     TRIM(ORDER_ID) AS order_id,
     CAST(TAX_RATE AS NUMBER(10,4)) AS tax_rate,
     TRIM(CLIENT_NAME) AS client_name,
-    TRY_TO_TIMESTAMP_NTZ(ORDER_AT) AS order_at,
+    ORDER_AT AS order_at,
     TRIM(SESSION_ID) AS session_id,
     TRIM(STATE) AS state,
     TRIM(PHONE) AS phone,
     TRIM(PAYMENT_INFO) AS payment_info,
     TRY_TO_DECIMAL(REGEXP_REPLACE(SHIPPING_COST, '[^0-9.]', ''), 10, 2) AS shipping_cost,
-    TRY_TO_BOOLEAN("_fivetran_deleted") AS fivetran_deleted,
-    TRY_TO_TIMESTAMP_NTZ(REPLACE("_fivetran_synced", ' Z', '')) AS fivetran_synced
+    "_fivetran_deleted" AS fivetran_deleted,
+    "_fivetran_synced" AS fivetran_synced
 FROM LOAD.WEB_SCHEMA.ORDERS
