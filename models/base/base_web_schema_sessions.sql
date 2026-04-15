64300b4e960ca655ed953caa24b6b2c2efd6ea52
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 11:06:39 -0400

    Create base_web_schema_sessions.sql

---
+++ models/base/base_web_schema_sessions.sql
@@ -0,0 +1,8 @@
+SELECT
+    "_fivetran_id" AS fivetran_id,
+    TRIM(PAGE_NAME) AS page_name,
+    VIEW_AT AS view_at,
+    TRY_TO_NUMBER(REGEXP_REPLACE(TRIM(SESSION_ID), '^s', '')) AS session_id,
+    "_fivetran_deleted" AS fivetran_deleted,
+    "_fivetran_synced" AS fivetran_synced
+FROM LOAD.WEB_SCHEMA.SESSIONS
