c8eece7e9d75c3b9afa56a9445812e9a2fff1d7b
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 13:08:24 -0400

    Update base_google_drive_hr_quit.sql

--- models/base/base_google_drive_hr_quit.sql
+++ models/base/base_google_drive_hr_quit.sql
@@ -2,7 +2,6 @@ SELECT
     _FILE AS file, 
     _LINE AS line, 
     _MODIFIED AS modified, 
-    _FIVETRAN_SYNCED AS fivetran_synced, 
     CAST(EMPLOYEE_ID AS NUMBER) AS employee_id, 
     CAST(QUIT_DATE AS DATE) AS quit_date 
 FROM LOAD.GOOGLE_DRIVE.HR_QUITS
