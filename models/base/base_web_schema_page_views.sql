e84414df522da8fc1d204528991737c13a88509b
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 11:01:17 -0400

    Update base_web_schema_page_views.sql

--- models/base/base_web_schema_page_views.sql
+++ models/base/base_web_schema_page_views.sql
@@ -1,8 +1,8 @@
 SELECT
     "_fivetran_id" AS fivetran_id,
     TRIM(PAGE_NAME) AS page_name,
-    TRY_TO_TIMESTAMP_NTZ(VIEW_AT) AS view_at,
+    VIEW_AT AS view_at,
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM(SESSION_ID), '^s', '')) AS session_id,
     "_fivetran_deleted" AS fivetran_deleted,
-    TRY_TO_TIMESTAMP_NTZ(REPLACE("_fivetran_synced", ' Z', '')) AS fivetran_synced
+    "_fivetran_synced" AS fivetran_synced
 FROM LOAD.WEB_SCHEMA.PAGE_VIEWS
