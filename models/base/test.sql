d93b49844a072c610b496d82c6dbfdd68a5df852
lewiseastwood476@gmail.com <lewiseastwood476@gmail.com>
2026-04-15 12:48:19 +0000

    j

---
+++ models/base/test.sql
@@ -0,0 +1,7 @@
+select current_role(), current_database(), current_schema();
+
+show grants to user <your_snowflake_username>;
+
+show grants on schema DEV.GROUP15_BASE;
+
+show grants on database DEV;
