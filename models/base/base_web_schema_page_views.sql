eb164bf7ca8c845e7154fbaeab778c076e1020a0
leweastwood73 <lewiseastwood476@gmail.com>
2026-04-15 13:05:55 -0400

    Update base_web_schema_page_views.sql

--- models/base/base_web_schema_page_views.sql
+++ models/base/base_web_schema_page_views.sql
@@ -1,7 +1,5 @@
 SELECT
     TRIM(PAGE_NAME) AS page_name,
     VIEW_AT AS view_at,
-    TRY_TO_NUMBER(REGEXP_REPLACE(TRIM(SESSION_ID), '^s', '')) AS session_id,
-    "_fivetran_deleted" AS fivetran_deleted,
-    "_fivetran_synced" AS fivetran_synced
+    TRY_TO_NUMBER(REGEXP_REPLACE(TRIM(SESSION_ID), '^s', '')) AS session_id
 FROM LOAD.WEB_SCHEMA.PAGE_VIEWS
