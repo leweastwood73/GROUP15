cd93f20366d4bdf35854fa3cc34cb150ad0fd989
aas2427-colum13 <aas2427@columbia.edu>
2026-04-14 20:45:51 -0400

    Create base_google_hr_returns.sql

---
+++ models/base/base_google_hr_returns.sql
@@ -0,0 +1,13 @@
+SELECT
+    _FILE AS file,
+    _LINE AS line,
+    _MODIFIED AS modified,
+    _FIVETRAN_SYNCED AS fivetran_synced,
+    RETURNED_AT AS returned_at,
+    ORDER_ID AS order_id,
+    CASE
+        WHEN LOWER(IS_REFUNDED) = 'yes' THEN TRUE
+        WHEN LOWER(IS_REFUNDED) = 'no' THEN FALSE
+        ELSE NULL
+    END AS is_refunded
+FROM LOAD.GOOGLE_DRIVE.RETURNS;
